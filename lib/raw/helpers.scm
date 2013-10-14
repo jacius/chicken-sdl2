5b4a870ab11ca078490884ce647121b9a3bba25b
John Croisant <john+chicken@croisant.net>
2013-10-14 17:48:07 -0400

    Added define-foreign-struct helper.

--- lib/raw/helpers.scm
+++ lib/raw/helpers.scm
@@ -69,6 +69,9 @@
        (foreign-lambda* void () body-string)))))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; ENUMS
+
 (define-syntax define-enum
   (syntax-rules ()
     ((define-enum enum-name value-name ...)
@@ -84,3 +87,85 @@
      (begin
        (export value-name ...)
        (define-enum enum-name value-name ...)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; STRUCTS
+
+
+(define-syntax define-foreign-struct
+  (syntax-rules ()
+    ((define-foreign-struct struct-type
+       (field-args ...)
+       ...)
+     (begin
+       (%define-foreign-struct-field struct-type field-args ...)
+       ...))))
+
+;;; I sure wish syntax-rules could easily use strings generated at
+;;; macro expansion time. Then I could do this:
+
+;; (define-syntax %define-foreign-struct-field
+;;   (syntax-rules ()
+;;     ;; getter and setter
+;;     ((%define-foreign-struct-field
+;;       struct-type field-type field-name
+;;       getter-name setter-name)
+;;      (begin
+;;        (define setter-name
+;;          (foreign-lambda*
+;;           void
+;;           ((struct-type obj) (field-type val))
+;;           ,(sprintf "~S->~S = ~S;" obj field-name val)))
+;;        (define getter-name
+;;          (getter-with-setter
+;;           (foreign-lambda*
+;;            field-type
+;;            ((struct-type obj))
+;;            ,(sprintf "C_return(~S->~S);" obj field-name))
+;;           setter-name))))
+;;     ;; no setter
+;;     ((%define-foreign-struct-field
+;;       struct-type field-type field-name
+;;       getter-name)
+;;      (define getter-name
+;;        (foreign-lambda*
+;;         field-type
+;;         ((struct-type obj))
+;;         ,(sprintf "C_return(~S->~S);" obj field-name))))))
+
+;;; ... instead of this:
+
+(define-syntax %define-foreign-struct-field
+  (ir-macro-transformer
+   (lambda (form inject compare?)
+     (let ((obj 'obj)
+           (val 'val)
+           (struct-type (inject (list-ref form 1)))
+           (field-type  (inject (list-ref form 2)))
+           (field-name  (inject (list-ref form 3)))
+           (getter-name (inject (list-ref form 4)))
+           (setter-name (and (> (length form) 5)
+                             (inject (list-ref form 5)))))
+       (if setter-name
+           ;; getter and setter
+           `(begin
+              (define ,(inject setter-name)
+                (foreign-lambda*
+                 void
+                 ((,(inject struct-type) ,(inject obj))
+                  (,(inject field-type) ,(inject val)))
+                 ,(sprintf "C_return(~S->~S);" obj field-name)))
+              (define ,(inject getter-name)
+                (getter-with-setter
+                 (foreign-lambda*
+                  ,(inject field-type)
+                  ((,(inject struct-type) ,(inject obj)))
+                  ,(sprintf "C_return(~S->~S);" obj field-name))
+                 ,(inject setter-name))))
+           ;; no setter
+           `(define ,(inject getter-name)
+              (foreign-lambda*
+               ,(inject field-type)
+               ((,(inject struct-type) ,(inject obj)))
+               ,(sprintf "C_return(~S->~S);" obj field-name))))))))
